ef8f1ea3e1b3b1689e4bc306428235a8d3742928
Christoph Muehlbacher <christoph.muehlbacher@gmx.at>
2023-11-30 16:29:02 +0100

    add f3 and f4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Git ist cool. Really? YESSSSSSSSSSSSSSSSS m1 f1 f2 r1");
+Console.WriteLine("Hello, World! Git ist cool. Really? YESSSSSSSSSSSSSSSSS m1 f1 f2 f3 f4 r1");
 
 
